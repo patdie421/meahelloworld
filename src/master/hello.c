552fa558a9a209d09d2f97669b6a0fd4a0e7c465
patrice.dietsch <patrice.dietsch@gmail.com>
2014-07-18 14:20:38 +0000

    Ajout de l'appel comio2

--- src/master/hello.c
+++ src/master/hello.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "comio2.h"
+
+static char *serial = "/dev/ttyATH0";
 
 int main(int argc, char *argv[])
 {
+   comio2_ad_t *ad=NULL;  
+
    printf("Hello, world (from ");
 
 #ifdef TECHNO_linux
@@ -13,4 +18,17 @@ int main(int argc, char *argv[])
 #endif
 
    printf(")\n");
+
+   ad=comio2_new_ad();
+   ret=comio2_init(ad, serial, B115200);
+   if(ret<0)
+   {
+      fprintf(stderr, "ERROR - %s ", serial);
+      perror("");
+      fprintf(stderr, "\n");
+      exit(1);
+   }
+   comio2_atCmdSend(ad, 1, "", 0);
+   
+   comio2_close(ad);
 }
